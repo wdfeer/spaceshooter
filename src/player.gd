4207490b9141e62e610b49372cfce8e2048a0b45
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-26 11:41:29 +0100

    WIP shooting

--- src/player.gd
+++ src/player.gd
@@ -1,8 +1,20 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
+const SHOOT_INVERVAL: float = 0.2
+
+@export
+var projectile_scene: PackedScene
+var shoot_timer: float = SHOOT_INVERVAL
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot") && can_shoot():
+		shoot()
+		shoot_timer = 0
 
 func _physics_process(delta: float) -> void:
+	shoot_timer += delta
+	
 	var vy := Input.get_axis("up", "down") 
 	var vx := Input.get_axis("left", "right")
 	velocity = Vector2(vx, vy) * SPEED
@@ -10,3 +22,10 @@ func _physics_process(delta: float) -> void:
 	
 	if velocity.length() > 0:
 		rotation = velocity.angle() + PI / 2
+
+func can_shoot() -> bool:
+	return shoot_timer >= SHOOT_INVERVAL
+func shoot():
+	var proj: Projectile = projectile_scene.instantiate()
+	proj.velocity = Vector2.from_angle(rotation)
+	get_parent().add_child(proj)
--- src/projectile.gd
+++ src/projectile.gd
@@ -1,10 +1,15 @@
+class_name Projectile
 extends Area2D
 
 var friendly = true
+var timeleft: float = 1.5
 var velocity: Vector2
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
+	timeleft -= delta
+	if timeleft < 0:
+		queue_free()
 
 func _on_area_entered(area: Area2D) -> void:
 	if can_hit(area):
