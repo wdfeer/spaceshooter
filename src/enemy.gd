331c821b0511349914145b01d8335ef911a9b1e8
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-26 12:01:55 +0100

    Kill enemies

--- src/enemy.gd
+++ src/enemy.gd
@@ -1,11 +1,12 @@
-extends CharacterBody2D
+extends Area2D
 
 const SPEED = 250.0
+var velocity: Vector2 = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	velocity = global_position.direction_to(get_player_position()) * SPEED
 	rotation = velocity.angle() + PI / 2
-	move_and_slide()
+	global_position += velocity * delta
 
 func get_player_position() -> Vector2:
 	var player: Node2D = $"../Player"
