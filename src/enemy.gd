d40553508a2f1657c52dab07318c25d2516995a1
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-26 11:59:39 +0100

    Enemy face player

--- src/enemy.gd
+++ src/enemy.gd
@@ -4,6 +4,7 @@ const SPEED = 250.0
 
 func _physics_process(delta: float) -> void:
 	velocity = global_position.direction_to(get_player_position()) * SPEED
+	rotation = velocity.angle() + PI / 2
 	move_and_slide()
 
 func get_player_position() -> Vector2:
