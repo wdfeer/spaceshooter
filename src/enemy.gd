cd10cd2518c7e011e0d5888b1db89a83fe502530
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-26 11:59:04 +0100

    Create enemy.tscn

---
+++ src/enemy.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+const SPEED = 250.0
+
+func _physics_process(delta: float) -> void:
+	velocity = global_position.direction_to(get_player_position()) * SPEED
+	move_and_slide()
+
+func get_player_position() -> Vector2:
+	var player: Node2D = $"../Player"
+	if player:
+		return player.global_position
+	return Vector2.ZERO
