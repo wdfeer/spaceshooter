8b537729850b917138153a871f62f8409e0294d0
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-26 15:20:12 +0100

    Refactor shooting

--- src/player.gd
+++ src/player.gd
@@ -4,20 +4,9 @@ extends Area2D
 const SPEED = 300.0
 var velocity = Vector2.ZERO
 
-const SHOOT_INVERVAL: float = 0.2
-@export
-var projectile_scene: PackedScene
-var shoot_timer: float = SHOOT_INVERVAL
-const SHOOT_VELOCITY: float = 600
 
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("shoot") && can_shoot():
-		shoot()
-		shoot_timer = 0
 
 func _physics_process(delta: float) -> void:
-	shoot_timer += delta
-	
 	var vy := Input.get_axis("up", "down") 
 	var vx := Input.get_axis("left", "right")
 	velocity = Vector2(vx, vy) * SPEED
@@ -25,11 +14,3 @@ func _physics_process(delta: float) -> void:
 	
 	if velocity.length() > 0:
 		rotation = velocity.angle() + PI / 2
-
-func can_shoot() -> bool:
-	return shoot_timer >= SHOOT_INVERVAL
-func shoot():
-	var proj: Projectile = projectile_scene.instantiate()
-	proj.global_position = global_position
-	proj.velocity = Vector2.from_angle(rotation - PI / 2) * SHOOT_VELOCITY
-	get_parent().add_child(proj)
---
+++ src/player_shooter.gd
@@ -0,0 +1,20 @@
+extends Timer
+
+const SHOOT_VELOCITY: float = 600
+
+@export
+var projectile_scene: PackedScene
+
+@onready
+var player: Player = $".."
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("shoot") && is_stopped():
+		shoot()
+
+func shoot():
+	var proj: Projectile = projectile_scene.instantiate()
+	proj.global_position = player.global_position
+	proj.velocity = Vector2.from_angle(player.rotation - PI / 2) * SHOOT_VELOCITY
+	get_parent().add_child(proj)
+	start()
