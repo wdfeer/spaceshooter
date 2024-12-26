3dc23a0e1f547ea11fcef03d6e91b0ab2f6b937d
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-26 16:20:54 +0100

    Load mars.tscn on touching mars

--- src/solar_system/solar_system.gd
+++ src/solar_system/solar_system.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
-
 func _on_area_exited(area: Area2D) -> void:
 	if area is Player:
 		area.global_position = global_position
+
+@export
+var mars_scene: PackedScene
+func _on_mars_loader_area_entered(area: Area2D) -> void:
+	if area is Player:
+		get_tree().change_scene_to_packed(mars_scene)
