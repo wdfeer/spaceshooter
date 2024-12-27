0040620fc67cd18e07bc9c126eab2a4e7047c247
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-27 11:07:24 +0100

    Load earth.tscn on Earth

--- src/solar_system/solar_system.gd
+++ src/solar_system/solar_system.gd
@@ -1,11 +1,18 @@
 extends Area2D
-pass # Replace with function body.
+
 func _on_area_exited(area: Area2D) -> void:
 	if area is Player:
 		area.global_position = global_position
 
 @export
 var mars_scene: PackedScene
+@export
+var earth_scene: PackedScene
+
 func _on_mars_loader_area_entered(area: Area2D) -> void:
 	if area is Player:
 		get_tree().change_scene_to_packed(mars_scene)
+
+func _on_earth_area_area_entered(area: Area2D) -> void:
+	if area is Player:
+		get_tree().change_scene_to_packed(earth_scene)
