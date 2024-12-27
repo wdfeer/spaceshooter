e6f9812182773233997765dfae9a266e3487ff42
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-27 11:33:25 +0100

    WIP Upgrades

--- src/user_data.gd
+++ src/user_data.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var kill_count: int = 0
+var upgrade_points: int = 0
 
 func _ready() -> void:
 	load_game()
@@ -8,8 +9,10 @@ func _ready() -> void:
 func save_game():
 	var file = FileAccess.open("user://save", FileAccess.WRITE)
 	file.store_32(kill_count)
+	file.store_32(upgrade_points)
 
 func load_game():
 	var file = FileAccess.open("user://save", FileAccess.READ)
 	if file:
 		kill_count = file.get_32()
+		upgrade_points = file.get_32()
