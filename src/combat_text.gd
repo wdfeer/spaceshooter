d0f93fd5238ebdc19545f0769b3a502f41fbde4c
wdfeer <49950781+wdfeer@users.noreply.github.com>
2024-12-29 19:41:19 +0100

    Update CombatText visual

--- src/combat_text.gd
+++ src/combat_text.gd
@@ -15,21 +15,22 @@ func _process(delta: float) -> void:
 	
 	for x in damage_numbers:
 		x.life -= delta
+		x.pos += DamageNumber.VELOCITY * delta
 	damage_numbers = damage_numbers.filter(func(x): return x.life > 0.0)
 	queue_redraw()
 
 func _draw() -> void:
 	for x in damage_numbers:
-		draw_string(font, x.pos, str(x.number))
+		draw_string(font, x.pos, str(x.number), 0, -1, 16, modulate.lerp(Color.TRANSPARENT, 1.0 - x.life))
 
 func damage_number(pos: Vector2, number: float):
 	var element = DamageNumber.new()
-	element.life = 1
 	element.pos = pos - global_position
 	element.number = number
 	damage_numbers.push_back(element)
 
 class DamageNumber:
-	var life: float
+	const VELOCITY = Vector2.UP * 60
+	var life: float = 1
 	var pos: Vector2
 	var number: float
